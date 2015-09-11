7570a953f96009aaa79cb778bcb34a3a6e9b883c
supernoir <kawohl.bentodesign@gmail.com>
2015-09-11 19:54:51 +0200

    Adds different views, e.g. jobs

--- app_refugio.rb
+++ app_refugio.rb
@@ -2,5 +2,19 @@ require 'sinatra'
 
 get '/' do
   @greetings = ["Welcome","Hos Geldiniz","أهلا وسهلا","Bienvenue"]
+
+  job_title = params[:job_title]
+  job_description = params[:job_description]
+  job_startdate = params[:job_startdate]
+  job_contact = params[:job_contact]
+
+  @job = {"Title" => job_title, "Description" => job_description, "Start Date" => job_startdate, "Contact" => job_contact}
+
+
   erb :index
+end
+
+get '/jobs' do
+
+  erb :jobs
 end
